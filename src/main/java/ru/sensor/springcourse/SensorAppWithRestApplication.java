7d9972b533626e024571ea45e2444843e7ab36dc
sergei_darin <swd86@mail.ru>
2024-05-23 15:59:52 +0400

    Initial commit

---
+++ src/main/java/ru/sensor/springcourse/SensorAppWithRestApplication.java
@@ -0,0 +1,13 @@
+package ru.sensor.springcourse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SensorAppWithRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SensorAppWithRestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/sensor/springcourse/SensorAppWithRestApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.sensor.springcourse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SensorAppWithRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
